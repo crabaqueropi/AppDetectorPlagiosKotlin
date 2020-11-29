292b737de157280b96e4b35f9d2ffeaf6a6851fc
crabaqueropi <crabaqueropi@unal.edu.co>
2020-11-28 21:04:15 -0500

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    private double xOffset;
+    private double yOffset;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        primaryStage.setTitle("Detector de Plagios - Kotlin");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
